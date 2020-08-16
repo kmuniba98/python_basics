8bdbf1913ebe981b94b6d4be60831c5126646588
kmuniba98 <mkhan@rollins.edu>
2020-08-16 02:58:26 -0400

    first commit

---
+++ python_basics_1.py
@@ -0,0 +1,23 @@
+
+# this is a comment-  it can be used to document and explain code
+# each line of a comment starts with a '#' and doesn't execute at runtime
+
+print('\nthis is a print statement:\nit shows up in the console\n')
+# '/n' is a newline character
+
+name = 'Kevin' # strings are in single or double quotes
+age = 22
+is_awesome = True # boolean is True or False
+
+# if/else using boolean
+if(is_awesome):
+    print('u r awesome sauce')
+else:
+    print('u r a loser')
+
+# if / elif / else using
+if(age < 18):
+
+elif(age == 21):
+
+else:
