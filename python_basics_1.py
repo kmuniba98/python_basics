c8912890b0d0f71a86c286460046ca1775843b8e
Kwt1997 <Kwt1997@yahoo.com>
2020-08-16 16:22:40 -0400

    first commit

--- python_basics_1.py
+++ python_basics_1.py
@@ -2,17 +2,18 @@
 # this is a comment-  it can be used to document and explain code
 # each line of a comment starts with a '#' and doesn't execute at runtime
 
-print('\nthis is a print statement:\nit shows up in the console\n')
+print('\nthis is a print statement:\tit shows up in the console\n')
 # '/n' is a newline character
 
 # python is super smart so you don't need to specify data types-
 # it will automatically detect it on its own
 name = 'Kevin' # strings are in single or double quotes
-is_awesome = True # boolean is True or False
+is_awesome = False # boolean is True or False
+bad_at_sex = True
 age = 22
 
 # if/else using boolean
-if(is_awesome):
+if(is_awesome and not bad_at_sex):
     print('u r awesome sauce')
 else:
     print('u r a loser')
@@ -30,6 +31,7 @@ elif(age == 22):
     print('i dont know about u but im feeling 22')
 else:
     print('every birthday is another year closer to death')
+# <, > <=, >=, ==
 
 print('\n')
 
@@ -43,4 +45,4 @@ print('\n')
 
 # or you can use 'range' to specify how many times you want to loop
 for i in range(10):
-    print(i)
+    print(friend)
